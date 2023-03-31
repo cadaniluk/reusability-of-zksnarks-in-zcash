7068cae180425b29bc03aeb00f966dc422a8c1ad
Cordian A. Daniluk <cordian.daniluk@grenoble-inp.org>
2023-03-31 14:09:46 +0200

    improve zcash.py

--- zcash.py
+++ zcash.py
@@ -5,16 +5,21 @@ from urllib.parse import urlencode
 from numpy import array_split
 from math import ceil
 
-try:
-    with open('api-key', 'r') as f:
-        api_key = f.read()
-except FileNotFoundError:
-    print('File "api-key" could not be opened.')
-    quit()
+def chunks(end, start=0, verbosity=1):
+    chunk_size = 100
+    for subseq in array_split(range(end, start - 1, -1), ceil((end - (start - 1)) / chunk_size)):
+        yield getblockio_req(*[{'method': 'getblock', 'params': [str(j), verbosity]} for j in subseq])
 
-url = 'https://zec.getblock.io/'
+def parse_range(range_str):
+    if '-' in sys.argv[2]:
+        start, end = list(map(int, range_str.split('-')))
+    else:
+        start = int(sys.argv[2])
+        end = start
+    return start, end
 
 def getblockio_req(*data):
+    url = 'https://zec.getblock.io/'
     header = {
         'x-api-key': api_key.strip(),
         'Content-Type': 'application/json'
@@ -29,43 +34,64 @@ def getblockio_req(*data):
     return json.loads(resp.read())
 
 def usage():
-    print(f'Usage:\n{sys.argv[0]} [<normal RPC>|find-anchor <height> <joinsplit-index>|getblock <start-end>]')
+    print(f'Usage:\n{sys.argv[0]} [<normal RPC>|find-anchor <height> <anchor>|getblock <start-end>]|find-shielded <start-end>|show-tx <txid>')
     quit()
 
 def main():
+    global api_key
+
     if len(sys.argv) == 1:
         usage()
+
+    try:
+        with open('api-key', 'r') as f:
+            api_key = f.read()
+    except FileNotFoundError:
+        print('File "api-key" could not be opened.')
+        quit()
     
     match sys.argv[1]:
         case 'getblock':
             verbosity = int(sys.argv[3]) if len(sys.argv) > 3 else 1
-            if '-' in sys.argv[2]:
-                start,end = list(map(int, sys.argv[2].split('-')))
-                resp = getblockio_req()
-            else:
-                start = int(sys.argv[2])
-                end = start
-    
+            start, end = parse_range(sys.argv[2])
             resp = getblockio_req(*[{'method': 'getblock', 'params': [str(i), verbosity]} for i in range(start, end + 1)])
             print(json.dumps(resp, indent=4))
         case 'find-anchor':
             # given a block height and an anchor, look for that anchor backwards from the given height
             if len(sys.argv) < 4:
                 usage()
-            chunk_size = 1000
             height = int(sys.argv[2])
             anchor = sys.argv[3]
-            for subseq in array_split(range(height, 0, -1), ceil(height / chunk_size)):
-                blocks = getblockio_req(*[{'method': 'getblock', 'params': [str(j), 1]} for j in subseq])
-                print('fetched new chunk')
+            for blocks in chunks(height):
+                print(f'Searching {blocks[-1]["result"]["height"]}-{blocks[0]["result"]["height"]}...')
                 for block in blocks:
-                    print(f'anchor={block["result"]["blockcommitments"]}')
-                    if block['result']['blockcommitments'] == anchor:
-                        print('Found.')
+                    print(f'block {block["result"]["height"]}: anchor={block["result"]["anchor"]}')
+                    if block['result']['anchor'] == anchor or block['result']['blockcommitments'] == anchor:
+                        print(f'Found anchor in block {block["result"]["height"]}.')
                         break
                 else:
                     continue
                 break
+        case 'find-shielded':
+            end, start = parse_range(sys.argv[2])
+            for blocks in chunks(end, start, verbosity=2):
+                print(f'Searching {blocks[-1]["result"]["height"]}-{blocks[0]["result"]["height"]}...')
+                for block in blocks:
+                    for tx in block['result']['tx']:
+                        if tx['vjoinsplit'] != [] or tx.get('vShieldedSpend', []) != [] \
+                                or tx.get('vShieldedOutput', []) != []:
+                            print(f'Found shielded transaction in block {block["result"]["height"]}, TX {tx["txid"]}.')
+                            break
+                    else:
+                        continue
+                    break
+                else:
+                    continue
+                break
+        case 'show-tx':
+            raw_tx = getblockio_req({'method': 'getrawtransaction', 'params': [sys.argv[2]]})
+            tx = getblockio_req({'method': 'decoderawtransaction', 'params': [raw_tx[0]['result']]})
+            print(json.dumps(tx, indent=4))
         case _:
             resp = getblockio_req({'method': sys.argv[1], 'params': sys.argv[2:]})
             print(json.dumps(resp, indent=4))
